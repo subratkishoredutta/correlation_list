d3023ac2e3cc705aff6cd7d61116b7a3feeb9d85
SUBRAT KISHORE DUTTA <subratkishoredutta1234@gmail.com>
2019-11-09 01:35:12 +0530

    commit 1
    corrlist(limit,source_data): function gives you the list of parameter pairs with correlation value greater than the limit.
    
    to import into the program write :
    from correlation_list import corrlist

---
+++ correlation_list.py
@@ -0,0 +1,17 @@
+def corrlist(limit,source_data):
+    i=0
+    j=0
+    highcorr=list()
+    corlist=list(source_data)
+    cordict=dict()
+    for i in range(0,len(corlist)):
+        cordict[i]= corlist[i]
+    
+    correlation=source_data.corr()
+    n = limit
+    for i in range(0,len(cordict)):
+        for j in range(1,len(cordict)):
+            if (correlation[cordict[i]][cordict[j]] > n) :
+                if (cordict[i]!=cordict[j]) and ((cordict[j],cordict[i]) in highcorr)==False :
+                    highcorr.append((cordict[i],cordict[j]))
+    print(highcorr)
